9239ddb1d9831003adf08b04e12561f3f9ecdd4c
Richard Sentino <richard@mingdinative.com>
2013-12-10 22:18:27 +1300

    Make task editable on the fly

--- public/js/angularjs-app/app.js
+++ public/js/angularjs-app/app.js
@@ -1,5 +1,11 @@
 
-var pingApp = angular.module('taskApp', [
+var taskApp = angular.module('taskApp', [
     'taskApp.controllers',
     'taskApp.taskFactory',
+    'xeditable',
+    'ngSanitize'
 ]);
+
+taskApp.run(function(editableOptions) {
+    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
